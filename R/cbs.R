274aec070a68c37071270bda8441b8bf5f51ff15
lbm364dl <geometrydashm364@gmail.com>
2025-03-25 11:51:01 +0100

    Use clearer column names

--- R/cbs.R
+++ R/cbs.R
@@ -10,5 +10,16 @@ get_processing_coefs <- function(file_path) {
   file_path |>
     readr::read_csv() |>
     dplyr::select(-Item, -Element) |>
-    dplyr::rename_with(tolower)
+    dplyr::rename_with(tolower) |>
+    dplyr::rename(
+      item_to_process = processeditem,
+      item_to_process_code = item_code,
+      value_to_process = value,
+      item_processed = item,
+      initial_conversion_factor = product_fraction,
+      initial_value_processed = value_proc_raw,
+      conversion_factor_scaling = scaling,
+      final_conversion_factor = cf,
+      final_value_processed = value_proc
+    )
 }
