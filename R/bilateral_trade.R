69acba7da44dbfb25307974a0afe6bb4e7ab6901
lbm364dl <geometrydashm364@gmail.com>
2025-03-28 17:09:01 +0100

    Read bilateral trade data and test

--- R/bilateral_trade.R
+++ R/bilateral_trade.R
@@ -1,35 +1,25 @@
 get_bilateral_trade <- function(file_path) {
-  df <-
-    file_path |>
+  file_path |>
     readr::read_csv() |>
     dplyr::rename_with(tolower) |>
-    # TODO: Check why NAs in item column
-    dplyr::filter(!is.na(item)) |>
-    dplyr::group_by(year, area_code, area, element, item, unit) |>
     dplyr::mutate(
-      my_cnt = dplyr::n(),
-      my_sum_value = sum(value),
-      my_country_share = value / sum(value),
-      total_coountry_share = sum(country_share),
+      unit = ifelse(unit == "Head", "heads", unit),
+      from_code = ifelse(element == "Export", area_code, area_code_p),
+      to_code = ifelse(element == "Export", area_code_p, area_code),
     ) |>
-    dplyr::arrange(.by_group = TRUE)
-    # dplyr::filter(my_cnt > 1, !dplyr::near(total_coountry_share, 1, 1e-6)) |>
-    # tidyr::nest()
+    prefer_flow_direction("Export") |>
+    dplyr::select(year, from_code, to_code, item, unit, value)
+}
 
-  browser()
+# Keep all rows with preferred direction (Import, Export)
+# when both of them exist. Otherwise use the one present.
+prefer_flow_direction <- function(bilateral_trade, direction) {
+  preferred_direction <- dplyr::filter(bilateral_trade, element == direction)
 
-  df |>
-    pointblank::create_agent() |>
-    pointblank::col_vals_expr(
-      ~ dplyr::near(my_country_share, country_share, 1e-6)
+  bilateral_trade |>
+    dplyr::anti_join(
+      preferred_direction,
+      by = c("from_code", "to_code", "year", "item")
     ) |>
-    pointblank::interrogate() |>
-    print()
-  # dplyr::select(-area, -area_p) |>
-  # dplyr::mutate(
-  #   from_code = ifelse(element == "Export", area_code, area_code_p),
-  #   to_code = ifelse(element == "Export", area_code_p, area_code),
-  #   .keep = "unused",
-  #   .before = 1
-  # )
+    dplyr::bind_rows(preferred_direction)
 }
--- tests/testthat/test_bilateral_trade.R
+++ tests/testthat/test_bilateral_trade.R
@@ -1,26 +1,74 @@
-testthat::test_that(
-  "get_bilateral_trade gives consistent country_share",
-  {
-    bilateral_trade_path <- here::here(
-      "inst/extdata/input/processed/bilateral_trade.csv"
-    )
-    if (!file.exists(bilateral_trade_path)) {
-      skip("Not running local test that depends on file")
-    }
-
-    bilateral_trade <- bilateral_trade_path |>
-      get_bilateral_trade() |>
-      dplyr::filter(from_code == 4, year == 1986) |>
-      dplyr::arrange(item) |>
-      dplyr::group_by(from_code, year, item) |>
-      dplyr::mutate(my_total = sum(value))
-
-    bilateral_trade |>
-      pointblank::create_agent() |>
-      pointblank::col_vals_expr(
-        ~ dplyr::near(sum(value) * country_share, value, tol = 1e-6)
-      ) |>
-      pointblank::interrogate() |>
-      print()
+testthat::test_that("get_bilateral_trade has consistent data", {
+  bilateral_trade_path <- here::here(
+    "inst/extdata/input/processed/bilateral_trade.csv"
+  )
+  if (!file.exists(bilateral_trade_path)) {
+    skip("Not running local test that depends on file")
   }
-)
+
+  bilateral_trade <- bilateral_trade_path |>
+    get_bilateral_trade() |>
+    is.na() |>
+    any() |>
+    testthat::expect_false()
+})
+
+testthat::test_that("prefer_flow_direction chooses preferred trade data", {
+  bilateral_trade <- tibble::tribble(
+    ~from_code, ~to_code, ~year, ~item, ~element, ~value,
+    1, 2, 2000, "item_1", "Import", 0,
+    1, 2, 2000, "item_1", "Export", 0,
+    1, 2, 2000, "item_2", "Export", 0,
+    1, 3, 2000, "item_2", "Import", 0,
+    2, 3, 2000, "item_2", "Import", 0,
+    2, 3, 2001, "item_2", "Export", 0,
+    2, 3, 2001, "item_2", "Import", 0,
+  ) |>
+    dplyr::arrange(from_code, to_code, year, item)
+
+  brute_group_by_result <-
+    bilateral_trade |>
+    dplyr::group_by(from_code, to_code, year, item) |>
+    dplyr::filter(dplyr::n() == 1 | element == "Import") |>
+    dplyr::ungroup() |>
+    dplyr::arrange(from_code, to_code, year, item)
+
+  my_result <- prefer_flow_direction(bilateral_trade, "Import") |>
+    dplyr::arrange(from_code, to_code, year, item)
+
+  expected_import_result <- tibble::tribble(
+    ~from_code, ~to_code, ~year, ~item, ~element, ~value,
+    1, 2, 2000, "item_1", "Import", 0,
+    1, 2, 2000, "item_2", "Export", 0,
+    1, 3, 2000, "item_2", "Import", 0,
+    2, 3, 2000, "item_2", "Import", 0,
+    2, 3, 2001, "item_2", "Import", 0,
+  ) |>
+    dplyr::arrange(from_code, to_code, year, item)
+
+  testthat::expect_equal(my_result, brute_group_by_result)
+  testthat::expect_equal(my_result, expected_import_result)
+
+  brute_group_by_result <-
+    bilateral_trade |>
+    dplyr::group_by(from_code, to_code, year, item) |>
+    dplyr::filter(dplyr::n() == 1 | element == "Export") |>
+    dplyr::ungroup() |>
+    dplyr::arrange(from_code, to_code, year, item)
+
+  my_result <- prefer_flow_direction(bilateral_trade, "Export") |>
+    dplyr::arrange(from_code, to_code, year, item)
+
+  expected_export_result <- tibble::tribble(
+    ~from_code, ~to_code, ~year, ~item, ~element, ~value,
+    1, 2, 2000, "item_1", "Export", 0,
+    1, 2, 2000, "item_2", "Export", 0,
+    1, 3, 2000, "item_2", "Import", 0,
+    2, 3, 2000, "item_2", "Import", 0,
+    2, 3, 2001, "item_2", "Export", 0,
+  ) |>
+    dplyr::arrange(from_code, to_code, year, item)
+
+  testthat::expect_equal(my_result, brute_group_by_result)
+  testthat::expect_equal(my_result, expected_export_result)
+})
