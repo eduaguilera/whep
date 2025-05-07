cf4a15c120a53afe9114ff681ed99ab619b76229
edu <eduardo.aguilera@csic.es>
2025-05-07 16:23:54 +0200

    Ensure the functions work with examples, and improve sum_fill function

--- R/gapfilling.R
+++ R/gapfilling.R
@@ -18,12 +18,14 @@ sample_tibble <- tibble::tibble(
   change_variable =c(1,1,1,1,1,1,0,0,0,0,0,0)
 )
 
-filled_tibble <- linear_fill(sample_tibble, value, year, category)
+linear_filled_tibble <- linear_fill(sample_tibble, value, year, category)
+proxy_filled_tibble <- proxy_fill(sample_tibble, value, proxy_variable, year, category)
+sum_filled_tibble <- sum_fill(sample_tibble, value, change_variable, category)
 
 # Fills gaps by linear interpolation, when possible, or carrying forward or backwards, when not possible, and labels output accordingly
 # The data should be he data when needed before running the function
-linear_fill <- function(data, var, time_index, ...) { # data= data frame; var = variable to be filled; time_index = time index (usually year); ... = grouping variables
-  data |> 
+linear_fill <- function(df, var, time_index, ...) { # df = data frame; var = variable to be filled; time_index = time index (usually year); ... = grouping variables
+  df |> 
     dplyr::group_by(...) |> 
     dplyr::mutate(
       Value_interpfilled = zoo::na.approx({{ var }},
@@ -59,18 +61,18 @@ linear_fill <- function(data, var, time_index, ...) { # data= data frame; var = 
 
 # Fills gaps by using changes in a proxy variable, using ratios between filled variable and proxy variable, and labels output accordingly
 # Remember to group the data when needed before running the function
-proxy_fill <- function(data, var, proxyvar, time_index, ...) { # data=dataframe, var=variable to be filled, proxyvar=variable used as proxy, Index=time index (usually year)
-  data %>%
-    dplyr::mutate(proxy_ratio = {{ var }} / {{ proxyvar }}) %>%
-    linear_fill(proxy_ratio, time_index) %>%
+proxy_fill <- function(df, var, proxy_var, time_index, ...) { # df = data frame, var = variable to be filled, proxy_var = variable used as proxy, time_index = time index (usually year); ... = grouping variables
+  df |>
+    dplyr::mutate(proxy_ratio = {{ var }} / {{ proxy_var }}) |>
+    linear_fill(proxy_ratio, {{ time_index }}, ...) |>
     dplyr::mutate(
       "Source_{{var}}" := ifelse(!is.na({{ var }}),
                                  "Original",
-                                 ifelse(Source_Proxy_ratio == "Linear interpolation",
+                                 ifelse(Source_proxy_ratio == "Linear interpolation",
                                         "Proxy interpolated",
-                                        ifelse(Source_Proxy_ratio == "Last value carried forward",
+                                        ifelse(Source_proxy_ratio == "Last value carried forward",
                                                "Proxy carried forward",
-                                               ifelse(Source_Proxy_ratio == "First value carried backwards",
+                                               ifelse(Source_proxy_ratio == "First value carried backwards",
                                                       "Proxy carried backwards",
                                                       NA
                                                )
@@ -79,17 +81,39 @@ proxy_fill <- function(data, var, proxyvar, time_index, ...) { # data=dataframe,
       ),
       "{{var}}" := ifelse(!is.na({{ var }}),
                           {{ var }},
-                          proxy_ratio * {{ proxyvar }}
+                          proxy_ratio * {{ proxy_var }}
       )
     )
 }
 
 # Function to fill gaps in a given variable (var) of a time series with the sum of the previous value of var and the value of another column (change_var). The values of var are accumulated along the series.
-sum_fill <- function(var, change_var) {
-  for (i in seq_along(var)) {
-    if (is.na(var[i]) && i > 1) {
-      var1[i] <- var1[i - 1] + change_var[i]
-    }
-  }
-  return(var)
+sum_fill <- function(df, var, change_var, ...) {
+  var_sym <- rlang::ensym(var)
+  change_var_sym <- rlang::ensym(change_var)
+  var_name <- rlang::as_string(var_sym)
+  change_var_name <- rlang::as_string(change_var_sym)
+  
+  group_syms <- rlang::ensyms(...)
+  
+  df <- dplyr::group_by(df, !!!group_syms) |>
+    dplyr::group_modify(function(.x, .y) {
+      var_vec <- .x[[var_name]]
+      change_vec <- .x[[change_var_name]]
+      
+      status_vec <- ifelse(is.na(var_vec), NA, "Original")
+      
+      for (i in seq_along(var_vec)) {
+        if (is.na(var_vec[i]) && i > 1) {
+          var_vec[i] <- var_vec[i - 1] + change_vec[i]
+          status_vec[i] <- "Filled with sum"
+        }
+      }
+      
+      .x[[var_name]] <- var_vec
+      .x[[paste0("Source_",var_name)]] <- status_vec
+      return(.x)
+    }) |>
+    dplyr::ungroup()
+  
+  return(df)
 }
