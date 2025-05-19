a34b131431d72ecd89d6b6bc165c703196b97f65
lbm364dl <geometrydashm364@gmail.com>
2025-05-19 12:21:54 +0200

    Add tests and docs

--- R/supply_use.R
+++ R/supply_use.R
@@ -1,3 +1,32 @@
+# TODO: Revisit the whole file. I'm pretty sure there are mistakes,
+# along with incomplete parts. It's better to first finish the model
+# and then come back here.
+
+#' Supply and use tables
+#'
+#' @description
+#' Create a table with processes, their inputs (_use_) and their
+#' outputs (_supply_).
+#'
+#' @returns
+#' A tibble with the supply and use data for processes.
+#' It contains the following columns:
+#' - `year`: The year in which the recorded event occurred.
+#' - `area`: The name of the country where the data is from.
+#' - `area_code`: FAOSTAT internal code for each country. Equivalences
+#'    with ISO 3166-1 numeric can be found in the _Area Codes_ CSV from the
+#'    zip file that can be downloaded from
+#'    [FAOSTAT](https://www.fao.org/faostat/en/#data/FBS). TODO: Think about
+#'    this, would be nice to use ISO3 codes but won't be enough for our periods
+#' - `item`: Natural language name for the item.
+#' - `item_code`: FAOSTAT internal code for each item.
+#'
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' build_supply_use()
+#' }
 build_supply_use <- function() {
   # TODO: There's a name mismatch in two items
   # CBS_item                  supply_process_item
@@ -6,69 +35,81 @@ build_supply_use <- function() {
   # Keep balance sheet names for now
 
   .build_supply_use_from_inputs(
-    supply_process_table = .get_processes_table("input/raw/items_supply.csv"),
-    use_process_table = .get_processes_table("input/raw/items_use.csv"),
+    supply_processes = .read_local_csv("input/raw/items_supply.csv"),
+    use_processes = .read_local_csv("input/raw/items_use.csv"),
     coeffs = get_processing_coefs(get_file_path("processing_coefs")),
     cbs = get_wide_cbs(get_file_path("commodity_balance_sheet"))
   )
 }
 
 .build_supply_use_from_inputs <- function(
-    supply_process_table,
-    use_process_table,
+    supply_processes,
+    use_processes,
     coeffs,
     cbs) {
-  processes_table <- use_process_table |>
+  processes_table <- .join_supply_use_processes(supply_processes, use_processes)
+
+  tibble::tibble() |>
+    .add_use_for_feed(processes_table) |>
+    .add_use_for_seed(processes_table, cbs) |>
+    .add_use_for_slaughtering(processes_table) |>
+    .add_supply_use_for_processing(processes_table, coeffs) |>
+    .add_rest_of_supply(supply_processes, cbs)
+}
+
+.join_supply_use_processes <- function(supply_processes, use_processes) {
+  use_processes |>
     dplyr::full_join(
-      supply_process_table,
+      supply_processes,
       by = c("proc", "proc_code"),
       relationship = "many-to-many",
       suffix = c("_to_process", "_processed")
     )
-
-  dplyr::bind_rows(
-    .add_use_for_feed(processes_table),
-    .add_use_for_seed(processes_table, cbs),
-    .add_use_for_slaughtering(processes_table),
-    .add_supply_use_for_processing(processes_table, coeffs)
-  )
 }
 
-.add_use_for_seed <- function(processes_table, cbs) {
-  processes <- processes_table |>
-    dplyr::filter(type == "seedwaste")
+.add_rest_of_supply <- function(supply_use, supply_process_table, cbs) {
+  items_done <- supply_use |>
+    dplyr::filter(type == "supply") |>
+    dplyr::distinct(item)
 
-  use <- processes |>
+  cbs |>
+    dplyr::anti_join(items_done, "item") |>
+    dplyr::inner_join(supply_process_table, "item") |>
+    dplyr::filter(domestic_supply > 0) |>
+    dplyr::select(year, area, proc, item, domestic_supply) |>
+    dplyr::rename(value = domestic_supply) |>
+    dplyr::mutate(type = "supply") |>
+    dplyr::bind_rows(supply_use)
+}
+
+.add_use_for_seed <- function(supply_use, processes_table, cbs) {
+  processes_table |>
+    dplyr::filter(type == "seedwaste") |>
     # TODO: compare with full_join, why seedwaste use on animal products?
     dplyr::inner_join(cbs, dplyr::join_by(item_code_to_process == item_code)) |>
     dplyr::filter(seed > 0) |>
     dplyr::select(year, area, proc, item_to_process, seed) |>
     dplyr::rename(item = item_to_process, value = seed) |>
-    dplyr::mutate(type = "use")
-
-  # supply <- processes |>
-  #   dplyr::inner_join(cbs, dplyr::join_by(item_code_processed == item_code)) |>
-  #   dplyr::filter(domestic_supply > 0) |>
-  #   dplyr::select(year, area, proc, item_processed, domestic_supply) |>
-  #   dplyr::rename(item = item_processed, value = domestic_supply) |>
-  #   dplyr::mutate(type = "supply")
-
-  dplyr::bind_rows(use)
+    dplyr::mutate(type = "use") |>
+    dplyr::bind_rows(supply_use)
 }
 
 # TODO: Treat slaughtering processes
 # (probably need conversion factor from Livestock Units to physical unit)
-.add_use_for_slaughtering <- function(processes_table) {
-  tibble::tibble()
+.add_use_for_slaughtering <- function(supply_use, processes_table) {
+  supply_use
 }
 
 # TODO: Treat animal feed use processes
 # Use feed intake data from Eduardo
-.add_use_for_feed <- function(processes_table) {
-  tibble::tibble()
+.add_use_for_feed <- function(supply_use, processes_table) {
+  supply_use
 }
 
-.add_supply_use_for_processing <- function(processes_table, coeffs) {
+.add_supply_use_for_processing <- function(
+    supply_use,
+    processes_table,
+    coeffs) {
   processes <- processes_table |>
     dplyr::filter(!type %in% c("feed", "seedwaste", "slaughtering")) |>
     # TODO: compare with full_join, deal with missing processes
@@ -86,11 +127,5 @@ build_supply_use <- function() {
     dplyr::rename(item = item_to_process, value = value_to_process) |>
     dplyr::mutate(type = "use")
 
-  dplyr::bind_rows(supply, use)
-}
-
-.get_processes_table <- function(csv_path) {
-  "extdata" |>
-    system.file(csv_path, package = utils::packageName()) |>
-    readr::read_csv(show_col_types = FALSE)
+  dplyr::bind_rows(supply, use, supply_use)
 }
--- tests/testthat/test_supply_use.R
+++ tests/testthat/test_supply_use.R
@@ -6,7 +6,7 @@ testthat::test_that(
       "p1", "i1", "i2", "processing",
       "p1", "i1", "i3", "processing",
       "p1", "i4", "i2", "processing",
-      "p2", "i1", "i5", "processing",
+      "p2", "i5", "i6", "processing",
       "ignore", "ignore", "ignore", "seedwaste",
       "ignore", "ignore", "ignore", "feed",
     )
@@ -16,20 +16,21 @@ testthat::test_that(
       2000, "a1", "i1", 10, "i2", 20,
       2000, "a1", "i1", 10, "i3", 30,
       2000, "a1", "i4", 20, "i2", 40,
-      2000, "a1", "i1", 30, "i5", 20
+      2000, "a1", "i5", 30, "i6", 20
     )
     expected <- tibble::tribble(
       ~year, ~area, ~proc, ~item, ~value, ~type,
       2000, "a1", "p1", "i1", 10, "use",
       2000, "a1", "p1", "i4", 20, "use",
-      2000, "a1", "p2", "i1", 30, "use",
+      2000, "a1", "p2", "i5", 30, "use",
       2000, "a1", "p1", "i2", 60, "supply",
       2000, "a1", "p1", "i3", 30, "supply",
-      2000, "a1", "p2", "i5", 20, "supply",
+      2000, "a1", "p2", "i6", 20, "supply",
     ) |>
       dplyr::arrange(year, area, proc, item, type)
 
-    .add_supply_use_for_processing(processes_table, coeffs) |>
+    tibble::tibble() |>
+      .add_supply_use_for_processing(processes_table, coeffs) |>
       dplyr::arrange(year, area, proc, item, type) |>
       testthat::expect_equal(expected)
   }
@@ -55,7 +56,45 @@ testthat::test_that(".add_use_for_seed works for 'seedwaste' use items", {
   ) |>
     dplyr::arrange(year, area, proc, item, type)
 
-  .add_use_for_seed(processes_table, cbs) |>
+  tibble::tibble() |>
+    .add_use_for_seed(processes_table, cbs) |>
     dplyr::arrange(year, area, proc, item, type) |>
     testthat::expect_equal(expected)
 })
+
+testthat::test_that(
+  ".add_rest_of_supply takes domestic supply of still non checked output items",
+  {
+    supply_process_table <- tibble::tribble(
+      ~proc, ~item,
+      "p1", "i1",
+      "p2", "i2",
+      "p3", "i3"
+    )
+    supply_use <- tibble::tribble(
+      ~year, ~area, ~proc, ~item, ~value, ~type,
+      2000, "a1", "p1", "i1", 10, "use",
+      2000, "a1", "p1", "i2", 10, "supply",
+    )
+    cbs <- tibble::tribble(
+      ~year, ~area, ~item, ~item_code, ~domestic_supply,
+      2000, "a1", "i1", 1, 30,
+      2000, "a1", "i2", 2, 40,
+      2000, "a1", "i3", 2, 50,
+    )
+    expected <- supply_use |>
+      dplyr::bind_rows(
+        tibble::tribble(
+          ~year, ~area, ~proc, ~item, ~value, ~type,
+          2000, "a1", "p1", "i1", 30, "supply",
+          2000, "a1", "p3", "i3", 50, "supply"
+        )
+      ) |>
+      dplyr::arrange(year, area, proc, item, type)
+
+    supply_use |>
+      .add_rest_of_supply(supply_process_table, cbs) |>
+      dplyr::arrange(year, area, proc, item, type) |>
+      testthat::expect_equal(expected)
+  }
+)
