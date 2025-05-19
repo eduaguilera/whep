6c98cd52fcb6148f6b568186d35b7b90b17f4ef2
lbm364dl <geometrydashm364@gmail.com>
2025-05-19 16:53:41 +0200

    Create functions for item codes and names

--- R/code_names.R
+++ R/code_names.R
@@ -21,14 +21,15 @@
 #' add_area_name(table)
 #'
 #' table |>
-#'  dplyr::rename(my_area_code = area_code) |>
-#'  add_area_name(code_column = "my_area_code")
+#'   dplyr::rename(my_area_code = area_code) |>
+#'   add_area_name(code_column = "my_area_code")
 #'
 #' add_area_name(table, name_column = "my_custom_name")
-add_area_name <- function(table, code_column = "area_code", name_column = "area_name") {
-  regions <- "input/raw/regions.csv" |>
-    .read_local_csv() |>
-    dplyr::select({{ name_column }} := name, {{ code_column }} := fabio_code)
+add_area_name <- function(
+    table,
+    code_column = "area_code",
+    name_column = "area_name") {
+  regions <- .get_regions(name_column, code_column)
 
   table |>
     dplyr::left_join(regions, {{ code_column }})
@@ -59,15 +60,100 @@ add_area_name <- function(table, code_column = "area_code", name_column = "area_
 #' add_area_code(table)
 #'
 #' table |>
-#'  dplyr::rename(my_area_name = area_name) |>
-#'  add_area_code(name_column = "my_area_name")
+#'   dplyr::rename(my_area_name = area_name) |>
+#'   add_area_code(name_column = "my_area_name")
 #'
 #' add_area_code(table, code_column = "my_custom_code")
-add_area_code <- function(table, name_column = "area_name", code_column = "area_code") {
-  regions <- "input/raw/regions.csv" |>
-    .read_local_csv() |>
-    dplyr::select({{ name_column }} := name, {{ code_column }} := fabio_code)
+add_area_code <- function(
+    table,
+    name_column = "area_name",
+    code_column = "area_code") {
+  regions <- .get_regions(name_column, code_column)
 
   table |>
     dplyr::left_join(regions, {{ name_column }})
+}
+
+#' Get item names from item codes
+#'
+#' @description
+#' Add a new column to an existing tibble with the corresponding name
+#' for each item code. The codes are assumed to be from those defined by
+#' FAOSTAT.
+#'
+#' @param table The table that will be modified with a new column.
+#' @param code_column The name of the column in `table` containing the codes.
+#' @param name_column The name of the output column containing the names.
+#'
+#' @returns A tibble with all the contents of `table` and an extra column
+#' named `name_column`, which contains the names. If there is no name match,
+#' an `NA` is included.
+#'
+#' @export
+#'
+#' @examples
+#' table <- tibble::tibble(item_code = c(2559, 2744, 9876))
+#' add_item_name(table)
+#'
+#' table |>
+#'   dplyr::rename(my_item_code = item_code) |>
+#'   add_item_name(code_column = "my_item_code")
+#'
+#' add_item_name(table, name_column = "my_custom_name")
+add_item_name <- function(
+    table,
+    code_column = "item_code",
+    name_column = "item_name") {
+  items <- .get_items(name_column, code_column)
+
+  table |>
+    dplyr::left_join(items, {{ code_column }})
+}
+
+#' Get item codes from item names
+#'
+#' @description
+#' Add a new column to an existing tibble with the corresponding code
+#' for each item name. The codes are assumed to be from those defined by
+#' the FAOSTAT.
+#'
+#' @param table The table that will be modified with a new column.
+#' @param code_column The name of the output column containing the codes.
+#' @param name_column The name of the column in `table` containing the names.
+#'
+#' @returns A tibble with all the contents of `table` and an extra column
+#' named `code_column`, which contains the codes. If there is no code match,
+#' an `NA` is included.
+#'
+#' @export
+#'
+#' @examples
+#' table <- tibble::tibble(item_name = c("Cottonseed", "Eggs", "Dummy Item"))
+#' add_item_code(table)
+#'
+#' table |>
+#'   dplyr::rename(my_item_name = item_name) |>
+#'   add_item_code(name_column = "my_item_name")
+#'
+#' add_item_code(table, code_column = "my_custom_code")
+add_item_code <- function(
+    table,
+    name_column = "item_name",
+    code_column = "item_code") {
+  items <- .get_items(name_column, code_column)
+
+  table |>
+    dplyr::left_join(items, {{ name_column }})
+}
+
+.get_regions <- function(name_column, code_column) {
+  "input/raw/regions.csv" |>
+    .read_local_csv() |>
+    dplyr::select(!!name_column := area, !!code_column := area_code)
+}
+
+.get_items <- function(name_column, code_column) {
+  "input/raw/items.csv" |>
+    .read_local_csv() |>
+    dplyr::select(!!name_column := item, !!code_column := item_code)
 }
--- tests/testthat/test_code_names.R
+++ tests/testthat/test_code_names.R
@@ -83,3 +83,81 @@ testthat::test_that("add_area_code correctly sets new column in table", {
       )
     )
 })
+
+testthat::test_that("add_item_name correctly sets new column in table", {
+  table <- tibble::tibble(item_code = c(2559, 2744, 9876))
+
+  table |>
+    add_item_name() |>
+    testthat::expect_equal(
+      tibble::tribble(
+        ~item_code, ~item_name,
+        2559, "Cottonseed",
+        2744, "Eggs",
+        9876, NA
+      )
+    )
+
+  table |>
+    dplyr::rename(dummy_code = item_code) |>
+    add_item_name(code_column = "dummy_code") |>
+    testthat::expect_equal(
+      tibble::tribble(
+        ~dummy_code, ~item_name,
+        2559, "Cottonseed",
+        2744, "Eggs",
+        9876, NA
+      )
+    )
+
+  table |>
+    dplyr::rename(dummy_code = item_code) |>
+    add_item_name(code_column = "dummy_code", name_column = "my_name") |>
+    testthat::expect_equal(
+      tibble::tribble(
+        ~dummy_code, ~my_name,
+        2559, "Cottonseed",
+        2744, "Eggs",
+        9876, NA
+      )
+    )
+})
+
+testthat::test_that("add_item_code correctly sets new column in table", {
+  table <- tibble::tibble(item_name = c("Cottonseed", "Eggs", "Dummy item"))
+
+  table |>
+    add_item_code() |>
+    testthat::expect_equal(
+      tibble::tribble(
+        ~item_name, ~item_code,
+        "Cottonseed", 2559,
+        "Eggs", 2744,
+        "Dummy item", NA
+      )
+    )
+
+  table |>
+    dplyr::rename(dummy_name = item_name) |>
+    add_item_code(name_column = "dummy_name") |>
+    testthat::expect_equal(
+      tibble::tribble(
+        ~dummy_name, ~item_code,
+        "Cottonseed", 2559,
+        "Eggs", 2744,
+        "Dummy item", NA
+      )
+    )
+
+  table |>
+    dplyr::rename(dummy_name = item_name) |>
+    add_item_code(name_column = "dummy_name", code_column = "dummy_code") |>
+    testthat::expect_equal(
+      tibble::tribble(
+        ~dummy_name, ~dummy_code,
+        "Cottonseed", 2559,
+        "Eggs", 2744,
+        "Dummy item", NA
+      )
+    )
+})
