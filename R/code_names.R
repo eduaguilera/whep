7f29a1f77ac974526030e8702ac05100125c4cd7
lbm364dl <geometrydashm364@gmail.com>
2025-05-20 10:29:34 +0200

    Create functions for process codes and names

--- R/code_names.R
+++ R/code_names.R
@@ -146,6 +146,80 @@ add_item_code <- function(
     dplyr::left_join(items, {{ name_column }})
 }
 
+#' Get process names from process codes
+#'
+#' @description
+#' Add a new column to an existing tibble with the corresponding name
+#' for each process code. The codes are assumed to be from those defined by
+#' the FABIO model.
+#'
+#' @param table The table that will be modified with a new column.
+#' @param code_column The name of the column in `table` containing the codes.
+#' @param name_column The name of the output column containing the names.
+#'
+#' @returns A tibble with all the contents of `table` and an extra column
+#' named `name_column`, which contains the names. If there is no name match,
+#' an `NA` is included.
+#'
+#' @export
+#'
+#' @examples
+#' table <- tibble::tibble(process_code = c("p017", "p076", "dummy"))
+#' add_process_name(table)
+#'
+#' table |>
+#'   dplyr::rename(my_process_code = process_code) |>
+#'   add_process_name(code_column = "my_process_code")
+#'
+#' add_process_name(table, name_column = "my_custom_name")
+add_process_name <- function(
+    table,
+    code_column = "process_code",
+    name_column = "process_name") {
+  processes <- .get_processes(name_column, code_column)
+
+  table |>
+    dplyr::left_join(processes, {{ code_column }})
+}
+
+#' Get process codes from process names
+#'
+#' @description
+#' Add a new column to an existing tibble with the corresponding code
+#' for each process name. The codes are assumed to be from those defined by
+#' the FABIO model.
+#'
+#' @param table The table that will be modified with a new column.
+#' @param code_column The name of the output column containing the codes.
+#' @param name_column The name of the column in `table` containing the names.
+#'
+#' @returns A tibble with all the contents of `table` and an extra column
+#' named `code_column`, which contains the codes. If there is no code match,
+#' an `NA` is included.
+#'
+#' @export
+#'
+#' @examples
+#' table <- tibble::tibble(
+#'   process_name = c("Beans production", "Olive Oil extraction", "Dummy")
+#' )
+#' add_process_code(table)
+#'
+#' table |>
+#'   dplyr::rename(my_process_name = process_name) |>
+#'   add_process_code(name_column = "my_process_name")
+#'
+#' add_process_code(table, code_column = "my_custom_code")
+add_process_code <- function(
+    table,
+    name_column = "process_name",
+    code_column = "process_code") {
+  processes <- .get_processes(name_column, code_column)
+
+  table |>
+    dplyr::left_join(processes, {{ name_column }})
+}
+
 .get_regions <- function(name_column, code_column) {
   "input/raw/regions.csv" |>
     .read_local_csv() |>
@@ -156,4 +230,10 @@ add_item_code <- function(
   "input/raw/items.csv" |>
     .read_local_csv() |>
     dplyr::select(!!name_column := item, !!code_column := item_code)
+}
+
+.get_processes <- function(name_column, code_column) {
+  "input/raw/processes.csv" |>
+    .read_local_csv() |>
+    dplyr::select(!!name_column := process, !!code_column := process_code)
 }
