8b011cd81ffb226941426fa2f03c62742d2542ef
Alejandra Fuentes <alejandra.fuentes@cchs.csic.es>
2025-06-23 15:01:36 +0200

    polities updated

--- inst/scripts/FT and WHEP.R
+++ inst/scripts/FT and WHEP.R
@@ -1,7 +1,12 @@
+# Merging Federico&Tena's dataset with WHEP polities dataset.
+# Alejandra Fuentes-Hinojosa - WHEP project
+# 06.23.2025 - IEGD/CCHS/CSIC
+
 library(sf)
 library(dplyr)
 library(tidyr)
 library(readxl)
+library(openxlsx)
 
 # Definir usuario
 user <- "Usuario"
@@ -29,7 +34,7 @@ federico_tena_clean <- federico_tena |>
   mutate(
     polity_code = NA,
     polity_name = NA,
-    polity_name_full = polity_name_FT_raw,  #polity_name_FT_raw -> polity_name_full
+    polity_name_full = polity_name_FT_raw, # polity_name_FT_raw -> polity_name_full
     polity_code_full = NA,
     polity_name_FAO = NA,
     polity_name_source = "FT",
@@ -47,7 +52,7 @@ federico_tena_clean <- federico_tena_clean |>
     polity_name_FT == "United States" ~ "United States of America",
     polity_name_FT == "Australia Commonwealth" ~ "Australia",
     polity_name_FT == "Cameroon (Kamerun)" ~ "Cameroon",
-    polity_name_FT == "Ceylon (Sri Lanka)" ~ "Sri Lanka", 
+    polity_name_FT == "Ceylon (Sri Lanka)" ~ "Sri Lanka",
     polity_name_FT == "Guinea Bisau (Portuguese Guinea)" ~ "Guinea-Bissau",
     polity_name_FT == "Hong-Kong" ~ "China, Hong Kong SAR",
     polity_name_FT == "South Australia" ~ "Australia",
@@ -60,21 +65,18 @@ federico_tena_clean <- federico_tena_clean |>
   mutate(
     polity_code = NA,
     polity_name = polity_name_FT,
-    polity_name_full = polity_name_full, #keeping the original
+    polity_name_full = polity_name_full, # keeping the original
     polity_code_full = NA,
     polity_name_FAO = NA,
     polity_name_source = "FT",
     polity_code_source = "FT"
   )
 
-# # WHEP data
-# whep_polities_clean <- whep_polities |>
-#   dplyr::mutate(
-#     polity_name_source = case_when(
-#       is.na(polity_code_source) | is.na(polity_name_source) ~ "WHEP",
-#       TRUE ~ polity_name_source
-#   )
-# )
+whep_polities_clean <- whep_polities |>
+  mutate(polity_name_source = case_when(
+    is.na(polity_code_source) | is.na(polity_name_source) ~ "WHEP",
+    TRUE ~ polity_name_source
+  ))
 
 ft_names <- federico_tena_clean %>%
   select(name_match = polity_name_FT, ft_name = polity_name_FT) %>%
@@ -82,72 +84,64 @@ ft_names <- federico_tena_clean %>%
   distinct()
 
 
+
 whep_polities_clean <- whep_polities_clean %>%
   left_join(ft_names, by = c("polity_name_full" = "name_match")) %>%
   left_join(ft_names, by = c("polity_name" = "name_match"), suffix = c("", "_alt")) %>%
   mutate(
-    polity_name_FT = coalesce(ft_name, ft_name_alt)  # Usar el nombre FT si existe coincidencia
+    polity_name_FT = coalesce(ft_name, ft_name_alt) # if there's matches
   ) %>%
   select(-ft_name, -ft_name_alt)
 
 
 new_ft <- federico_tena_clean$polity_name_FT[
-  !federico_tena_clean$polity_name_FT %in% c(whep_polities_clean$polity_name_full,
-                                             whep_polities_clean$polity_name)
+  !federico_tena_clean$polity_name_FT %in% c(
+    whep_polities_clean$polity_name_full,
+    whep_polities_clean$polity_name
+  )
 ]
 
 federico_new <- federico_tena_clean %>%
-  filter(polity_name_FT %in% new_ft)  
+  filter(polity_name_FT %in% new_ft)
 
-# # Identify polity_name_FT that don't exist in WHEP
-# new_ft <- federico_tena_clean$polity_name_FT[
-#   !federico_tena_clean$polity_name_FT %in% c(whep_polities_clean$polity_name_full,
-#                                              whep_polities_clean$polity_name)
-# ]
-# 
-# # Filter only new FT records
-# federico_new <- federico_tena_clean |>
-#   filter(polity_name_FT %in% new_ft)
 
-#MERGING
-final_columns <- c("polity_code", "polity_name", "polity_name_full",
-                   "polity_code_full", "polity_name_FAO", "polity_name_FT",
-                   "polity_name_source", "polity_code_source", "start_year",
-                   "end_year", "Comments FT")
+# MERGING
+final_columns <- c(
+  "polity_code", "polity_name", "polity_name_full",
+  "polity_code_full", "polity_name_FAO", "polity_name_FT",
+  "polity_name_source", "polity_code_source", "start_year",
+  "end_year", "Comments FT"
+)
 
 
 whep_raw <- whep_polities_clean
 
-# Asegura que la columna existe
 if (!("polity_name_FT" %in% names(whep_raw))) {
   whep_raw$polity_name_FT <- NA_character_
 }
-  
-  whep_raw <- dplyr::select(whep_raw, any_of(final_columns))
-  
-  missing_cols <- setdiff(final_columns, names(whep_raw))
-                  for (col in missing_cols) {
-                  whep_raw[[col]] <- NA
+
+whep_raw <- dplyr::select(whep_raw, any_of(final_columns))
+
+missing_cols <- setdiff(final_columns, names(whep_raw))
+for (col in missing_cols) {
+  whep_raw[[col]] <- NA
 }
 
-  whep_final <- dplyr::select(whep_raw, all_of(final_columns))
+whep_final <- dplyr::select(whep_raw, all_of(final_columns))
 
 # Adjust Federico-Tena to have all columns
 
 federico_final <- dplyr::select(federico_new, any_of(final_columns))
-#   
-# 
+
+
 missing_cols <- setdiff(final_columns, names(federico_final))
-                 for (col in missing_cols) {
-                   federico_final[[col]] <- NA
-                 }
-   
+for (col in missing_cols) {
+  federico_final[[col]] <- NA
+}
+
 federico_final <- dplyr::select(federico_final, all_of(final_columns))
 
 
-
-  
-  
 # merging the datasets
 polities_whep <- bind_rows(whep_final, federico_final)
 
@@ -162,7 +156,5 @@ polities_whep <- polities_whep |>
   )
 
 
-# Save the dataset
-output_path <- paste0("C:/Users/", user, "/Desktop/WHEP/inst/extdata/output/polities_whep.csv")
-write.xlsx(polities_whep, output_path, rowNames = FALSE)
-
+output <- paste0("C:/Users/", user, "/Desktop/WHEP/inst/extdata/output/polities_whep.csv")
+write.csv(polities_whep, output, row.names = FALSE)
