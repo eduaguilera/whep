85c3756284f88e97b1de9e9b8dfc0d9b596189dc
lbm364dl <geometrydashm364@gmail.com>
2025-06-26 13:30:13 +0200

    Add tests for supply use

--- tests/testthat/test_supply_use.R
+++ tests/testthat/test_supply_use.R
@@ -1,100 +1,167 @@
-testthat::test_that(
-  ".add_supply_use_for_processing works for 'processing' use items",
-  {
-    processes_table <- tibble::tribble(
-      ~proc, ~item_to_process, ~item_processed, ~type,
-      "p1", "i1", "i2", "processing",
-      "p1", "i1", "i3", "processing",
-      "p1", "i4", "i2", "processing",
-      "p2", "i5", "i6", "processing",
-      "ignore", "ignore", "ignore", "seedwaste",
-      "ignore", "ignore", "ignore", "feed",
-    )
-    coeffs <- tibble::tribble(
-      ~year, ~area, ~item_to_process, ~value_to_process,
-      ~item_processed, ~final_value_processed,
-      2000, "a1", "i1", 10, "i2", 20,
-      2000, "a1", "i1", 10, "i3", 30,
-      2000, "a1", "i4", 20, "i2", 40,
-      2000, "a1", "i5", 30, "i6", 20
-    )
-    expected <- tibble::tribble(
-      ~year, ~area, ~proc, ~item, ~value, ~type,
-      2000, "a1", "p1", "i1", 10, "use",
-      2000, "a1", "p1", "i4", 20, "use",
-      2000, "a1", "p2", "i5", 30, "use",
-      2000, "a1", "p1", "i2", 60, "supply",
-      2000, "a1", "p1", "i3", 30, "supply",
-      2000, "a1", "p2", "i6", 20, "supply",
-    ) |>
-      dplyr::arrange(year, area, proc, item, type)
+.expect_equal_unordered <- function(actual, expected) {
+  actual <- actual |>
+    dplyr::select(sort(names(actual))) |>
+    dplyr::arrange(dplyr::across(everything()))
 
-    tibble::tibble() |>
-      .add_supply_use_for_processing(processes_table, coeffs) |>
-      dplyr::arrange(year, area, proc, item, type) |>
-      testthat::expect_equal(expected)
+  expected <- expected |>
+    dplyr::select(sort(names(actual))) |>
+    dplyr::arrange(dplyr::across(everything()))
+
+  testthat::expect_equal(actual, expected)
+}
+
+testthat::test_that(
+  ".build_processing gives supply use table for processed items",
+  {
+    coeffs <- tibble::tribble(
+      ~year, ~area_code, ~item_cbs_code_to_process, ~value_to_process,
+      ~item_cbs_code_processed, ~final_value_processed,
+      2000, 1, 1, 10, 2, 20,
+      2000, 1, 1, 10, 3, 30,
+      2000, 1, 4, 20, 2, 40,
+      2000, 2, 5, 30, 6, 20
+    )
+
+    expected <- tibble::tribble(
+      ~year, ~area_code, ~proc_group, ~proc_cbs_code, ~item_cbs_code,
+      ~value, ~type,
+      2000, 1, "processing", 1, 2, 20, "supply",
+      2000, 1, "processing", 1, 3, 30, "supply",
+      2000, 1, "processing", 1, 1, 10, "use",
+      2000, 1, "processing", 4, 2, 40, "supply",
+      2000, 1, "processing", 4, 4, 20, "use",
+      2000, 2, "processing", 5, 6, 20, "supply",
+      2000, 2, "processing", 5, 5, 30, "use"
+    )
+
+    coeffs |>
+      .build_processing() |>
+      .expect_equal_unordered(expected)
   }
 )
 
-testthat::test_that(".add_use_for_seed works for 'seedwaste' use items", {
-  processes_table <- tibble::tribble(
-    ~proc, ~item_to_process, ~item_code_to_process, ~item_processed, ~type,
-    "p1", "i1", 1, "i1", "seedwaste",
-    "p2", "i2", 2, "i3", "seedwaste",
-    "ignore", "ignore", 3, "ignore", "slaughtering",
-    "ignore", "ignore", 4, "ignore", "feed",
-  )
-  cbs <- tibble::tribble(
-    ~year, ~area, ~item, ~item_code, ~seed,
-    2000, "a1", "i1", 1, 10,
-    2000, "a1", "i2", 2, 20,
-  )
-  expected <- tibble::tribble(
-    ~year, ~area, ~proc, ~item, ~value, ~type,
-    2000, "a1", "p1", "i1", 10, "use",
-    2000, "a1", "p2", "i2", 20, "use",
-  ) |>
-    dplyr::arrange(year, area, proc, item, type)
+testthat::test_that(
+  ".build_use_husbandry gives feed intake needs for animal husbandry",
+  {
+    feed_intake <- tibble::tribble(
+      ~year, ~area_code, ~live_anim_code, ~item_cbs_code, ~supply,
+      2000, 1, 1, 2, 20,
+      2000, 1, 1, 3, 40,
+      2000, 2, 4, 6, 40,
+      2000, 2, 5, 6, 20,
+      2001, 2, 5, 7, 20
+    )
 
-  tibble::tibble() |>
-    .add_use_for_seed(processes_table, cbs) |>
-    dplyr::arrange(year, area, proc, item, type) |>
-    testthat::expect_equal(expected)
-})
+    expected <- tibble::tribble(
+      ~year, ~area_code, ~proc_cbs_code, ~item_cbs_code, ~value, ~type,
+      2000, 1, 1, 2, 20, "use",
+      2000, 1, 1, 3, 40, "use",
+      2000, 2, 4, 6, 40, "use",
+      2000, 2, 5, 6, 20, "use",
+      2001, 2, 5, 7, 20, "use"
+    )
+
+    feed_intake |>
+      .build_use_husbandry() |>
+      .expect_equal_unordered(expected)
+  }
+)
 
 testthat::test_that(
-  ".add_rest_of_supply takes domestic supply of still non checked output items",
+  ".build_supply_husbandry gives livestock and their products",
   {
-    supply_process_table <- tibble::tribble(
-      ~proc, ~item,
-      "p1", "i1",
-      "p2", "i2",
-      "p3", "i3"
-    )
-    supply_use <- tibble::tribble(
-      ~year, ~area, ~proc, ~item, ~value, ~type,
-      2000, "a1", "p1", "i1", 10, "use",
-      2000, "a1", "p1", "i2", 10, "supply",
-    )
-    cbs <- tibble::tribble(
-      ~year, ~area, ~item, ~item_code, ~domestic_supply,
-      2000, "a1", "i1", 1, 30,
-      2000, "a1", "i2", 2, 40,
-      2000, "a1", "i3", 2, 50,
-    )
-    expected <- supply_use |>
-      dplyr::bind_rows(
-        tibble::tribble(
-          ~year, ~area, ~proc, ~item, ~value, ~type,
-          2000, "a1", "p1", "i1", 30, "supply",
-          2000, "a1", "p3", "i3", 50, "supply"
-        )
-      ) |>
-      dplyr::arrange(year, area, proc, item, type)
+    husbandry_items <- tibble::tibble(live_anim_code = c(1, 2, 4))
 
-    supply_use |>
-      .add_rest_of_supply(supply_process_table, cbs) |>
-      dplyr::arrange(year, area, proc, item, type) |>
-      testthat::expect_equal(expected)
+    primary_prod <- tibble::tribble(
+      ~year, ~area_code, ~item_prod_code, ~item_cbs_code, ~live_anim_code,
+      ~unit, ~value,
+      2000, 1, 1, 2, NA, "LU", 20,
+      2000, 1, 3, 4, NA, "LU", 30,
+      2000, 1, 5, 6, 1, "tonnes", 40,
+      2000, 1, 7, 8, 1, "tonnes", 45,
+      2001, 1, 1, 2, NA, "LU", 40,
+      2001, 1, 7, 8, 1, "tonnes", 50,
+      2001, 1, 7, 9, NA, "tonnes", 50,
+      2002, 1, 7, 2, 3, "tonnes", 50
+    )
+
+    expected <- tibble::tribble(
+      ~year, ~area_code, ~proc_cbs_code, ~item_cbs_code, ~value, ~type,
+      2000, 1, 2, 2, 13, "supply",
+      2000, 1, 4, 4, 19.5, "supply",
+      2000, 1, 1, 6, 40, "supply",
+      2000, 1, 1, 8, 45, "supply",
+      2001, 1, 2, 2, 26, "supply",
+      2001, 1, 1, 8, 50, "supply"
+    )
+
+    husbandry_items |>
+      .build_supply_husbandry(primary_prod) |>
+      .expect_equal_unordered(expected)
+  }
+)
+
+testthat::test_that(
+  ".build_use_crop_production gives seed use for crop production",
+  {
+    cbs_items <- tibble::tibble(item_cbs_code = c(1, 3, 8))
+
+    cbs <- tibble::tribble(
+      ~year, ~area_code, ~item_cbs_code, ~seed,
+      2000, 1, 1, 20,
+      2000, 1, 3, 25,
+      2001, 1, 8, 30,
+      2001, 1, 9, 30
+    )
+
+    expected <- tibble::tribble(
+      ~year, ~area_code, ~proc_cbs_code, ~item_cbs_code, ~value, ~type,
+      2000, 1, 1, 1, 20, "use",
+      2000, 1, 3, 3, 25, "use",
+      2001, 1, 8, 8, 30, "use"
+    )
+
+    cbs_items |>
+      .build_use_crop_production(cbs) |>
+      .expect_equal_unordered(expected)
+  }
+)
+
+testthat::test_that(
+  ".build_supply_crop_production gives crops and their residues",
+  {
+    crop_prod_items <- tibble::tibble(item_prod_code = c(1, 2, 4))
+
+    primary_prod <- tibble::tribble(
+      ~year, ~area_code, ~item_prod_code, ~item_cbs_code, ~live_anim_code,
+      ~unit, ~value,
+      2000, 1, 1, 3, NA, "tonnes", 40,
+      2000, 1, 2, 3, NA, "tonnes", 45,
+      2000, 1, 4, 5, NA, "tonnes", 50,
+      2001, 1, 4, 5, NA, "tonnes", 60,
+      2001, 1, 6, 3, NA, "tonnes", 60
+    )
+
+    crop_residues <- tibble::tribble(
+      ~year, ~area_code, ~item_cbs_code_crop, ~item_cbs_code_residue, ~value,
+      2000, 1, 3, 10, 40,
+      2000, 1, 5, 11, 45,
+      2001, 1, 5, 12, 60,
+      2001, 1, 4, 10, 80
+    )
+
+    expected <- tibble::tribble(
+      ~year, ~area_code, ~proc_cbs_code, ~item_cbs_code, ~value, ~type,
+      2000, 1, 3, 3, 85, "supply",
+      2000, 1, 5, 5, 50, "supply",
+      2001, 1, 5, 5, 60, "supply",
+      2000, 1, 3, 10, 40, "supply",
+      2000, 1, 5, 11, 45, "supply",
+      2001, 1, 5, 12, 60, "supply"
+    )
+
+    crop_prod_items |>
+      .build_supply_crop_production(primary_prod, crop_residues) |>
+      .expect_equal_unordered(expected)
   }
 )
