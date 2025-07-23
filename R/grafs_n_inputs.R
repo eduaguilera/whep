fa2f3da5b3c2c59b20693195c2f31c4591ac8e4e
Alice-Beckmann <alice.beckmann@cchs.csic.es>
2025-07-23 17:03:32 +0200

    Simplifying script

--- R/grafs_n_inputs.R
+++ R/grafs_n_inputs.R
@@ -31,18 +31,15 @@ create_n_inputs_grafs_spain <- function() {
   data <- .load_inputs_n_inputs()
 
   # Calculate N inputs and manure
-  n_inputs_prepared <- .calculate_n_inputs(
+  n_soil_inputs <- .calculate_n_inputs(
     data$n_balance_ygpit_all,
     data$codes_coefs
   )
 
-  # Summarise inputs
-  n_inputs_summary <- .summarise_inputs(n_inputs_prepared)
-
   # Summarise production
   n_inputs_combined <- .summarise_production(
     data$grafs_prod_destiny,
-    n_inputs_summary
+    n_soil_inputs
   )
 
   # Calculate NUE
@@ -57,21 +54,25 @@ create_n_inputs_grafs_spain <- function() {
 .load_inputs_n_inputs <- function() {
   result <-
     list(
-      n_Excretion_ygs =
-        readRDS(get_file_path("n_excretion_ygs")) |> dplyr::ungroup(),
       # TODO: Excretion need to be added to dataset as an input of Livestock
-      n_balance_ygpit_all =
-        readRDS(get_file_path("n_balance_ygpit_all")) |> dplyr::ungroup(),
-      grafs_prod_destiny =
-        readr::read_csv(get_file_path("GRAFS_prod_destiny_git")),
-      codes_coefs =
-        readxl::read_excel(
-          get_file_path("codes_coefs"),
-          sheet = "Names_biomass_CB"
-        )
+      n_Excretion_ygs = readRDS(
+        get_file_path("n_excretion_ygs")
+      ) |>
+        dplyr::ungroup(),
+      n_balance_ygpit_all = readRDS(
+        get_file_path("n_balance_ygpit_all")
+      ) |> dplyr::ungroup(),
+      grafs_prod_destiny = readr::read_csv(
+        get_file_path("GRAFS_prod_destiny_git")
+      ),
+      codes_coefs = readxl::read_excel(
+        get_file_path("codes_coefs"),
+        sheet = "Names_biomass_CB"
+      )
     )
   result
 }
+
 
 #' @title Assign some special items to Boxes -----------------------------------
 #' @return A named list with assigned items.
@@ -103,7 +104,7 @@ create_n_inputs_grafs_spain <- function() {
 #' @param n_balance_ygpit_all A data frame containing nitrogen balance data.
 #' @param codes_coefs A data frame merging biomass names to item names.
 #'
-#' @return A list with two tibbles: 'n_inputs_summary' and 'manure_summary'.
+#' @return One tibble: 'n_soil_inputs'
 #' @keywords internal
 #' @noRd
 .calculate_n_inputs <- function(
@@ -116,10 +117,10 @@ create_n_inputs_grafs_spain <- function() {
 
   # Merge Name_biomass with Item
   items <- codes_coefs |>
-    dplyr::select(Name_biomass, Item)
+    dplyr::distinct(Name_biomass, Item)
 
   # Combine all necessary n Inputs
-  n_inputs_summary <- n_balance_ygpit_all |>
+  n_soil_inputs <- n_balance_ygpit_all |>
     dplyr::left_join(items, by = "Name_biomass") |>
     dplyr::mutate(
       Item = ifelse(Name_biomass %in% firewood_biomass, "Firewood", Item),
@@ -127,97 +128,33 @@ create_n_inputs_grafs_spain <- function() {
         "semi_natural_agroecosystems", LandUse
       )
     ) |>
-    dplyr::group_by(Year, Province_name, Name_biomass, Item, Box) |>
-    dplyr::summarise(
-      Deposition = sum(Deposition, na.rm = TRUE),
-      BNF = sum(BNF, na.rm = TRUE),
-      Synthetic = sum(Synthetic, na.rm = TRUE),
-      Urban = sum(Urban, na.rm = TRUE),
-      .groups = "drop"
-    ) |>
-    dplyr::select(
-      Year, Province_name, Name_biomass, Item, Box, Deposition,
-      BNF, Synthetic, Urban
-    )
-
-  # Calculation of Manure from Excreta, Solid, Liquid
-  item_lookup <- codes_coefs |>
-    dplyr::select(Name_biomass, Item) |>
-    dplyr::distinct()
-
-  manure_selected <- n_balance_ygpit_all |>
-    dplyr::left_join(item_lookup, by = "Name_biomass") |>
-    dplyr::mutate(
-      Item = ifelse(Name_biomass %in% firewood_biomass, "Firewood", Item),
-      Box = ifelse(LandUse %in% semi_natural_agroecosystems,
-        "semi_natural_agroecosystems", LandUse
-      )
-    ) |>
-    dplyr::select(
-      Year, Province_name, Name_biomass, Item, Box, Excreta,
-      Solid, Liquid
-    )
-
-  manure_summary <- manure_selected |>
-    dplyr::group_by(Year, Province_name, Name_biomass, Item, Box) |>
-    dplyr::summarise(
-      Total_Manure = sum(Excreta + Solid + Liquid, na.rm = TRUE),
-      .groups = "drop"
-    )
-
-  list(
-    n_inputs_summary = n_inputs_summary,
-    manure_summary = manure_summary
-  )
-}
-
-#' @title Combine all Inputs ---------------------------------------------------
-#' @description Combines different N input sources and calculates total values
-#' per year, province, item, and box.
-#'
-#' @param n_inputs_prepared A list with 'n_inputs_summary' and 'manure_summary'
-#'
-#' @return A tibble summarising nitrogen input components by item and region.
-#' @keywords internal
-#' @noRd
-.summarise_inputs <- function(
-  n_inputs_prepared
-) {
-  n_inputs <- dplyr::full_join(
-    n_inputs_prepared$n_inputs_summary,
-    n_inputs_prepared$manure_summary,
-    by = c("Year", "Province_name", "Name_biomass", "Item", "Box")
-  )
-
-  # Summing Inputs for each Year, Province_name, Box
-  n_inputs_sum <- n_inputs |>
-    dplyr::group_by(Year, Province_name, Item, Box) |>
     dplyr::summarise(
       MgN_dep = sum(Deposition, na.rm = TRUE),
       MgN_fix = sum(BNF, na.rm = TRUE),
       MgN_syn = sum(Synthetic, na.rm = TRUE),
-      MgN_manure = sum(Total_Manure, na.rm = TRUE),
+      MgN_manure = sum(Excreta + Solid + Liquid, na.rm = TRUE),
       MgN_urban = sum(Urban, na.rm = TRUE),
-      .groups = "drop"
+      .by = c(Year, Province_name, Item, Box)
     ) |>
-    dplyr::ungroup()
+    dplyr::arrange(Year, Province_name)
 
-  n_inputs_sum
+  n_soil_inputs
 }
+
 
 #' @title GRAFS_Prod_Destiny ---------------------------------------------------
 #' @description Summarizes and calculates new columns: Prod_MgN
 #' Spread Destiny column to separate columns for Food, Feed, Other_uses, Export.
 #'
 #' @param grafs_prod_destiny Data containing production values by destiny.
-#' @param n_inputs_sum A tibble of summarized N inputs.
+#' @param n_soil_inputs A tibble of summarized N inputs.
 #'
 #' @return A tibble with combined N input and production data.
 #' @keywords internal
 #' @noRd
 .summarise_production <- function(
   grafs_prod_destiny,
-  n_inputs_sum
+  n_soil_inputs
 ) {
   grafs_prod_destiny_summary <- grafs_prod_destiny |>
     tidyr::pivot_wider(
@@ -242,7 +179,7 @@ create_n_inputs_grafs_spain <- function() {
 
   # Combine with n_inputs dataset
   n_inputs_combined <- dplyr::full_join(
-    n_inputs_sum, grafs_prod_destiny_summary,
+    n_soil_inputs, grafs_prod_destiny_summary,
     by = c("Year", "Province_name", "Item", "Box")
   ) |>
     dplyr::filter(!is.na(Box)) |>
