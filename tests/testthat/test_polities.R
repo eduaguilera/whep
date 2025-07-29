3d75469f1a21f11428b600a6e128de23a82d22df
lbm364dl <ctl.covaci@gmail.com>
2025-07-29 17:17:24 +0200

    Add integrity tests

---
+++ tests/testthat/test_polities.R
@@ -0,0 +1,28 @@
+testthat::test_that("get_polities is consistent", {
+  cols <- c(
+    "polity_code",
+    "polity_name",
+    "display_code",
+    "start_year",
+    "end_year",
+    "m49_code"
+  )
+
+  get_polities() |>
+    pointblank::expect_col_exists(all_of(cols)) |>
+    pointblank::expect_col_is_integer(
+      c("polity_code", "start_year", "end_year")
+    ) |>
+    pointblank::expect_col_is_character(
+      c("polity_name", "display_code", "m49_code")
+    ) |>
+    pointblank::expect_col_vals_not_null(
+      setdiff(cols, "m49_code")
+    ) |>
+    pointblank::expect_rows_distinct(polity_code) |>
+    pointblank::expect_rows_distinct(display_code) |>
+    pointblank::expect_rows_distinct(
+      m49_code,
+      preconditions = \(df) df |> dplyr::filter(!is.na(m49_code))
+    )
+})
