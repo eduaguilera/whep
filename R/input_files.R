7a70c71a4fcb0209ef798eba2ea7746d4397cd73
lbm364dl <ctl.covaci@gmail.com>
2025-08-01 13:30:19 +0200

    Change also different file

--- R/input_files.R
+++ R/input_files.R
@@ -57,8 +57,8 @@ whep_read_file <- function(file_alias, type = "parquet", version = NULL) {
   version <- .choose_version(file_info$version, version)
 
   file_info |>
-    purrr::pluck("board_url") |>
-    .build_board_with_progress() |>
+    purrr::pluck("board_url") |> 
+    .build_board_with_progress () |>
     pins::pin_download(file_alias, version = version) |>
     .read_file(type)
 }
