29c366dce600e710fa10372cd1dafd7782824574
lbm364dl <ctl.covaci@gmail.com>
2026-03-23 17:27:46 +0100

    Fix tests

--- data-raw/table_mappings.R
+++ data-raw/table_mappings.R
@@ -5,8 +5,7 @@ items_prod <- here::here("inst", "extdata", "items_prod.csv") |>
   readr::read_csv()
 
 polities <- here::here("inst", "extdata", "regions.csv") |>
-  readr::read_csv() |>
-  dplyr::mutate(area_code = as.character(area_code))
+  readr::read_csv()
 
 usethis::use_data(items_cbs, overwrite = TRUE)
 usethis::use_data(items_prod, overwrite = TRUE)
