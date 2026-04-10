01054411515a7f4593f4e95ff2b30194910f5acf
lbm364dl <ctl.covaci@gmail.com>
2026-04-10 09:58:00 +0200

    Test whole pipeline

--- inst/scripts/test.R
+++ inst/scripts/test.R
@@ -2,18 +2,22 @@
 # Run interactively to verify the complete data pipeline with real data.
 
 # 1. Primary production & commodity balance sheets
-prod <- build_primary_production(start_year = 1961)
-cbs <- build_commodity_balances(prod, start_year = 1961)
+prod <- build_primary_production()
+cbs <- build_commodity_balances(prod)
 
 # 2. Trade
 trade <- build_detailed_trade(cbs = cbs, extend_time = TRUE)
 
 # 3. Prices
 trade_prices <- build_trade_prices()
+
+vop <- whep_read_file("faostat-value-of-production")
 primary_prices <- build_primary_prices(
   primary_prod = prod,
-  trade_prices = trade_prices
+  trade_prices = trade_prices,
+  value_of_production = vop
 )
+
 cbs_prices <- build_cbs_prices(
   cbs = cbs,
   trade_prices = trade_prices
